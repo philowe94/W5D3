ec5b1df9a9bc1ceb7f6634f7321e6eb3678fcf66
juliajykim <jkim24@gwu.edu>
2021-03-24 14:45:55 -0400

    Start Question class

---
+++ AAQuestions/questions.rb
@@ -0,0 +1,33 @@
+require 'sqlite3'
+require 'singleton'
+
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class Question
+  def self.find_by_id(id)
+    questions = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE 
+        id = ?
+    SQL
+
+    Question.new(questions.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end
+end
