5abfa408e4e73ec89acce8176f160fc030c8cfd3
Philip Lowe <philip.lowe94@gmail.com>
2021-03-24 14:26:41 -0400

    designed AAQuestions schema

---
+++ AAQuestions/import_db.sql
@@ -0,0 +1,66 @@
+PRAGMA foreign_keys = ON;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS questions;
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS question_follows;
+
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+DROP TABLE IF EXISTS replies;
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    body TEXT NOT NULL,
+    question_id INTEGER NOT NULL,
+    parent_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (parent_id) REFERENCES replies(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+INSERT INTO
+    users (fname, lname)
+VALUES
+    ('Philip', 'Lowe'),
+    ('Julia', 'Kim');
+
+INSERT INTO
+    questions (title, body, author_id)
+VALUES
+    ('SQL', 'How do I make a database :(', (SELECT id FROM users WHERE fname = 'Philip'));
